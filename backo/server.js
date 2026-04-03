cdbab00669f498bb7dfd6e02cad01784e70e4428
Arshath <elyasarshath6963gmail.com>
2026-04-03 20:42:24 +0530

    error clear

--- backo/server.js
+++ backo/server.js
@@ -1,23 +1,19 @@
-require("dotenv").config(); 
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
-const { Server } = require("socket.io"); 
+const { Server } = require("socket.io");
 
 
 const app = express();
-const server = http.createServer(app); 
-const io = new Server(server, { cors: { origin: "*" } }); 
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected "))
-  .catch((err) => console.log("MongoDB error ", err));
 
-
-const locationRoutes = require("./routes/bus")(io);
+const busRoutes = require("./routes/bus")(io);
 app.use("/api/bus", busRoutes);
 
 io.on("connection", (socket) => {
@@ -26,6 +22,10 @@ io.on("connection", (socket) => {
     console.log("Client disconnected ", socket.id);
   });
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected "))
+  .catch((err) => console.log("MongoDB error ", err));
 
 server.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000 🚀");
